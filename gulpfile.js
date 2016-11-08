d1f96195656f466cee557bcaf1aea1b1aad1e6b4
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-11-08 16:23:45 +0900

    react読み込んでみた

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ let gulp = require('gulp');
 let runSequence = require('run-sequence');
 let browserify = require('browserify');
 let source = require('vinyl-source-stream');
+let babelify = require("babelify");
 
 // default
 gulp.task('default', ['build']);
@@ -17,8 +18,8 @@ gulp.task('build', () => {
 gulp.task('browserify', () => {
     browserify({
         entries: ['./js/index.js'],
-        require: ['jquery', 'underscore','backbone', 'bootstrap', 'backbone.validation']
     })
+    .transform(babelify, { presets: ["es2015", "react"] })
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./js/'));
---
+++ js/components/ReactSample.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var ReactSample = React.createClass({
+    render:function() {
+        return <h1>React Sample</h1>
+    }
+});
+
+module.exports = ReactSample;
